771d3a5bc2d873981fb24d3d8a7c79361345bc1f
Andrew Minion <andrew@andrewrminion.com>
2017-10-12 14:50:40 -0400

    initial commit

---
+++ cf7-redirect-parameters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Contact Form 7 Redirect with Parameters
+ * Plugin URI: https://github.com/macbookandrew/cf7-redirect-parameters
+ * Description: Adds options for Contact Form 7 forms to pass contact form data to another URL
+ * Version: 1.0.0
+ * Author: Andrew Minion
+ * Author URI: https://andrewrminion.com
+ * Copyright: 2017 Andrew Minion
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined( 'ABSPATH' ) or die( 'No access allowed' );
+
